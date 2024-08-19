2aa2e074a6b4919c13e67e3b02a62017953c9077
JoseNeto <blablabla123@gmail.com>
2024-08-19 12:41:14 -0400

    Adicionei o Ola fernando no codigo

--- meu_primeiro_projeto_java/src/App.java
+++ meu_primeiro_projeto_java/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá, Antonio");
+        System.out.println("Olá, Fernando");
     }
 }
